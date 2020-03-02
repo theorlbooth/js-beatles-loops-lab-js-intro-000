9c50f506dc93c45a51ccaf95a037befdbdddc596
Theo Booth <theorlbooth@googlemail.com>
2020-03-02 11:06:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < len(musicians)-1; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  console.log(array)
+  }
+}
